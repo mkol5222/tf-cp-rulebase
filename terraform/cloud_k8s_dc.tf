53a8cab42e231266399f1b4e804cc4d2d266cc5f
Martin Koldovsky <mkoldov@gmail.com>
2023-03-16 16:23:43 +0100

    .

--- terraform/cloud_k8s_dc.tf
+++ terraform/cloud_k8s_dc.tf
@@ -59,6 +59,101 @@ resource "checkpoint_management_kubernetes_data_center_server" "aks1" {
 
 }
 
+resource "checkpoint_management_data_center_query" "pods-default-ns" {
+  name         = "Pods in default NS"
+  data_centers = [checkpoint_management_kubernetes_data_center_server.aks1.name]
+
+
+    query_rules {
+    key_type = "tag"
+    key      = "__namespace"
+    values   = ["default"]
+  }
+  lifecycle {
+    ignore_changes = [
+
+      query_rules,
+    ]
+  }
+}
+
+resource "checkpoint_management_data_center_query" "superman-default" {
+  name         = "Superman(default)"
+  data_centers = [checkpoint_management_kubernetes_data_center_server.aks1.name]
+
+  query_rules {
+    key_type = "tag"
+    key      = "app"
+    values   = ["superman"]
+  }
+
+    query_rules {
+    key_type = "tag"
+    key      = "__namespace"
+    values   = ["default"]
+  }
+  lifecycle {
+    ignore_changes = [
+
+      query_rules,
+    ]
+  }
+}
+
+resource "checkpoint_management_data_center_query" "webka1-demo-prod" {
+  name         = "Webka1(demo) prod"
+  data_centers = [checkpoint_management_kubernetes_data_center_server.aks1.name]
+
+  query_rules {
+    key_type = "tag"
+    key      = "app"
+    values   = ["webka1"]
+  }
+  query_rules {
+    key_type = "tag"
+    key      = "env"
+    values   = ["prod"]
+  }
+    query_rules {
+    key_type = "tag"
+    key      = "__namespace"
+    values   = ["demo"]
+  }
+  lifecycle {
+    ignore_changes = [
+
+      query_rules,
+    ]
+  }
+}
+
+resource "checkpoint_management_data_center_query" "webka1-demo-test" {
+  name         = "Webka1(demo) test"
+  data_centers = [checkpoint_management_kubernetes_data_center_server.aks1.name]
+
+  query_rules {
+    key_type = "tag"
+    key      = "app"
+    values   = ["webka1"]
+  }
+  query_rules {
+    key_type = "tag"
+    key      = "env"
+    values   = ["test"]
+  }
+    query_rules {
+    key_type = "tag"
+    key      = "__namespace"
+    values   = ["demo"]
+  }
+  lifecycle {
+    ignore_changes = [
+
+      query_rules,
+    ]
+  }
+}
+
 resource "checkpoint_management_data_center_query" "aks1-test-web1" {
   name         = "AKS1 webka1 in test"
   data_centers = [checkpoint_management_kubernetes_data_center_server.aks1.name]
